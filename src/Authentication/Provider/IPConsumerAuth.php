1ae633420cb05ae1e0319b30d6a887fb259e60a0
enzo - Eduardo Garcia <enzo@anexusit.com>
2014-12-28 10:41:08 -0600

    first commit

---
+++ src/Authentication/Provider/IPConsumerAuth.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ip_consumer_auth\Authentication\Provider\IPConsumerAuth.
+ */
+
+namespace Drupal\ip_consumer_auth\Authentication\Provider;
+
+use \Drupal\Component\Utility\String;
+use Drupal\Core\Authentication\AuthenticationProviderInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Flood\FloodInterface;
+use Drupal\user\UserAuthInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * HTTP Basic authentication provider.
+ */
+class IPConsumerAuth implements AuthenticationProviderInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The user auth service.
+   *
+   * @var \Drupal\user\UserAuthInterface
+   */
+  protected $userAuth;
+
+  /**
+   * The flood service.
+   *
+   * @var \Drupal\Core\Flood\FloodInterface
+   */
+  protected $flood;
+
+  /**
+   * The entity manager.
+   *
+   * @var \Drupal\Core\Entity\EntityManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * Constructs a HTTP basic authentication provider object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Drupal\user\UserAuthInterface $user_auth
+   *   The user authentication service.
+   * @param \Drupal\Core\Flood\FloodInterface $flood
+   *   The flood service.
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   *   The entity manager service.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, EntityManagerInterface $entity_manager) {
+    $this->configFactory = $config_factory;
+    $this->entityManager = $entity_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(Request $request) {
+    // If Authentication Provider is enabled always apply
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function authenticate(Request $request) {
+    $allowed_ip_consumers = $this->configFactory->get('ip_consumer_auth.consumers_form_config')->get('allowed_ip_consumers');
+    $ips = explode( "\n", $allowed_ip_consumers );
+    $consumer_ip = $request->getClientIp(TRUE);
+    if (in_array($consumer_ip, $ips)) {
+      // Return Annonymous user
+      return $this->entityManager->getStorage('user')->load(0);
+    }
+    else{
+      throw new AccessDeniedHttpException();
+      return null;
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function cleanup(Request $request) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function handleException(GetResponseForExceptionEvent $event) {
+    $exception = $event->getException();
+    if ($exception instanceof AccessDeniedHttpException) {
+      $event->setException(new UnauthorizedHttpException('Invalid consumer origin.', $exception));
+      return TRUE;
+    }
+    return FALSE;
+  }
+
+}
---
+++ src/Form/ConsumersForm.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\ip_consumer_auth\Form\ConsumersForm.
+ */
+
+namespace Drupal\ip_consumer_auth\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class ConsumersForm extends ConfigFormBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormID() {
+    return 'consumers_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('ip_consumer_auth.consumers_form_config');
+    $form['allowed_ip_consumers'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Allowed IP Consumers'),
+      '#description' => $this->t('Place IP addresses on separate lines'),
+      '#default_value' => $config->get('allowed_ip_consumers'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    return parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('ip_consumer_auth.consumers_form_config')
+          ->set('allowed_ip_consumers', $form_state->getValue('allowed_ip_consumers'))
+        ->save();
+  }
+}
