0a56a2cca1d47ec86aa8b0dbac20f2088cbe9629
enzo - Eduardo Garcia <enzo@anexusit.com>
2015-05-25 22:13:57 -0600

    Update form

--- src/Form/ConsumersForm.php
+++ src/Form/ConsumersForm.php
@@ -19,26 +19,31 @@ class ConsumersForm extends ConfigFormBase
   public function getFormID() {
     return 'consumers_form';
   }
-
-
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
-    return ['ip_consumer_auth.consumers_form_config'];
+  public function  getEditableConfigNames() {
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('ip_consumer_auth.consumers_form_config');
+    $config = \Drupal::configFactory()->get('ip_consumer_auth.consumers_form_config');
+
     $form['allowed_ip_consumers'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Allowed IP Consumers'),
       '#description' => $this->t('Place IP addresses on separate lines'),
       '#default_value' => $config->get('allowed_ip_consumers'),
     ];
+
+    $form['debug'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Debug IP invalid request'),
+      '#default_value' => $config->get('debug'),
+    );
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -55,8 +60,9 @@ class ConsumersForm extends ConfigFormBase
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-    $this->config('ip_consumer_auth.consumers_form_config')
-          ->set('allowed_ip_consumers', $form_state->getValue('allowed_ip_consumers'))
-        ->save();
+    $config = \Drupal::configFactory()->getEditable('ip_consumer_auth.consumers_form_config');
+    $config->set('allowed_ip_consumers', $form_state->getValue('allowed_ip_consumers'));
+    $config->set('debug', $form_state->getValue('debug'));
+    $config->save();
   }
 }
